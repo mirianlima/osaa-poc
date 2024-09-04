57b336a12958ea046b62df3359ff10b5d5065e4a
mirian lima <mlima@mirians-MacBook-Air.local>
2024-09-04 10:28:46 -0400

    extracted s3 interaction functions into utils.py

--- src/osaa_pipeline/ingest/run.py
+++ src/osaa_pipeline/ingest/run.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
 import boto3
-import osaa_pipeline.config as config
+from osaa_pipeline.utils import upload_s3_client
+import osaa_pipeline.config as config 
 
 load_dotenv()
 
@@ -13,34 +14,19 @@ s3_client = boto3.client(
     region_name=os.getenv('REGION')
 )
 
-# Access configurations
-raw_data_dir = config.RAW_DATA_DIR
-s3_bucket_name = config.S3_BUCKET_NAME
-landing_area_folder = config.LANDING_AREA_FOLDER
-file_to_s3_folder_mapping = config.FILE_TO_S3_FOLDER_MAPPING
-
-# Load files to s3 buckets
+# Load files to S3 buckets
 def upload_files_to_s3():
-    for local_file_name, s3_sub_folder in file_to_s3_folder_mapping.items():
-        # Construct local folder path based on the S3 sub-folder mapping
-        local_folder_path = os.path.join(raw_data_dir, s3_sub_folder)
-        
-        # Construct the full local file path
-        local_file_path = os.path.join(local_folder_path, local_file_name)
-        
-        # Construct S3 object key (path within the bucket)
-        s3_object_key = f'{landing_area_folder}/{s3_sub_folder}/{local_file_name}'
-        
-        try:
-            # Check if local file exists
-            if os.path.isfile(local_file_path):
-                # Upload the file to S3
-                s3_client.upload_file(local_file_path, s3_bucket_name, s3_object_key)
-                print(f'Successfully uploaded {local_file_name} to s3://{s3_bucket_name}/{s3_object_key}')
-            else:
-                print(f'File not found: {local_file_path}')
-        except Exception as e:
-            print(f'Error uploading {local_file_name} to S3: {e}')
+    """
+    Uploads files from the local raw_data directory to specified landing folders in the S3 bucket.
+    """
+    # Use config variables directly in the function call
+    upload_s3_client(
+        s3_client=s3_client, 
+        local_data_dir=config.RAW_DATA_DIR, 
+        s3_bucket_name=config.S3_BUCKET_NAME, 
+        landing_area_folder=config.LANDING_AREA_FOLDER, 
+        file_to_s3_folder_mapping=config.FILE_TO_S3_FOLDER_MAPPING
+    )
 
 if __name__ == '__main__':
     upload_files_to_s3()
--- src/osaa_pipeline/utils.py
+++ src/osaa_pipeline/utils.py
@@ -1,4 +1,11 @@
 import os
+import logging
+import boto3
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+### INGEST ###
 
 def generate_file_to_s3_folder_mapping(raw_data_dir):
     """
@@ -20,3 +27,65 @@ def generate_file_to_s3_folder_mapping(raw_data_dir):
             file_to_s3_folder_mapping[file_name] = sub_folder
 
     return file_to_s3_folder_mapping
+
+### AWS S3 ###
+
+def upload_s3_client(s3_client, local_data_dir, s3_bucket_name, landing_area_folder, file_to_s3_folder_mapping):
+    """
+    Uploads files from the local data directory to specified folders in an S3 bucket.
+
+    :param s3_client: The boto3 S3 client.
+    :param local_data_dir: The base directory containing the local files.
+    :param s3_bucket_name: The name of the S3 bucket.
+    :param landing_area_folder: The landing area folder in the S3 bucket.
+    :param file_to_s3_folder_mapping: Mapping of file names to S3 subfolder names.
+    """
+    for local_file_name, s3_sub_folder in file_to_s3_folder_mapping.items():
+        # Construct the full local file path
+        local_file_path = os.path.join(local_data_dir, s3_sub_folder, local_file_name)
+        
+        # Construct the S3 object key (path within the bucket)
+        s3_object_key = f'{landing_area_folder}/{s3_sub_folder}/{local_file_name}'
+        
+        try:
+            # Check if local file exists
+            if os.path.isfile(local_file_path):
+                # Upload the file to S3
+                s3_client.upload_file(local_file_path, s3_bucket_name, s3_object_key)
+                logging.info(f'Successfully uploaded {local_file_name} to s3://{s3_bucket_name}/{s3_object_key}')
+            else:
+                logging.warning(f'File not found: {local_file_path}')
+        except Exception as e:
+            logging.error(f'Error uploading {local_file_name} to S3: {e}', exc_info=True)
+
+def download_s3_client(s3_client, s3_bucket_name, s3_folder, local_dir):
+    """
+    Downloads all files from a specified S3 folder to a local directory.
+
+    :param s3_client: The boto3 S3 client.
+    :param s3_bucket_name: The name of the S3 bucket.
+    :param s3_folder: The folder within the S3 bucket to download files from.
+    :param local_dir: The local directory to save downloaded files.
+    """
+    try:
+        # Ensure the local directory exists
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir)
+
+        # List objects in the specified S3 folder
+        response = s3_client.list_objects_v2(Bucket=s3_bucket_name, Prefix=s3_folder)
+        
+        # Check if any objects are returned
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                s3_key = obj['Key']
+                local_file_path = os.path.join(local_dir, os.path.basename(s3_key))
+                
+                # Download the file
+                s3_client.download_file(s3_bucket_name, s3_key, local_file_path)
+                logging.info(f'Successfully downloaded {s3_key} to {local_file_path}')
+        else:
+            logging.warning(f'No files found in s3://{s3_bucket_name}/{s3_folder}')
+
+    except Exception as e:
+        logging.error(f'Error downloading files from S3: {e}', exc_info=True)
