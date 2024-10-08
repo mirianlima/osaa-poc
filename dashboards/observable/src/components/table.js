73fd422cb50b95cbf39f7984ae966f61546a0c83
mirianlima <mspencerlima@gmail.com>
2024-10-08 14:28:00 -0400

    gitignore updated

--- dashboards/observable/src/components/table.js
+++
@@ -1,5 +0,0 @@
-import {DuckDBClient} from "npm:@observablehq/duckdb";
-
-const db = DuckDBClient.of({gaia: FileAttachment("datalake/staging/master/master.parquet")});
-
-//${Inputs.table(db, {rows: 16})}
--- dashboards/observable/src/data/missingLoader.csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import sys
-
-# Read the CSV
-df = pd.read_csv("src/data/missing.csv")
-# Write to CSV
-pd.to_csv(sys.stdout)
